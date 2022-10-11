5ea124ff71e295fb1599a3732f28ebb4acc1752a
Vincil Lau <vincillau@outlook.com>
2022-10-11 15:26:49 +0800

    fix: 修正xmake.lua中的项目名称

--- xmake.lua
+++ xmake.lua
@@ -12,7 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-set_project("dss")
+set_project("mydss")
 VERSION = "0.1.0"
 set_version(VERSION)
 set_languages("c++17")
